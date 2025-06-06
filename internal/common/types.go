9eca113fd37cc0600e72bd237f98c8e55ccf995c
Phani K <192951055+phani-kb@users.noreply.github.com>
2025-06-05 20:50:53 -0400

    Add common types and downloader interface for processing files

---
+++ internal/common/types.go
@@ -0,0 +1,45 @@
+package common
+
+import (
+	"fmt"
+)
+
+type Named interface {
+	GetName() string
+}
+
+type SourceType struct {
+	Name      string     `json:"name"`
+	ListTypes []ListType `json:"list_types,omitempty"`
+}
+
+func (st *SourceType) Validate() error {
+	if st.Name == "" {
+		return fmt.Errorf("name is required")
+	}
+	return nil
+}
+
+func (st *SourceType) GetListTypes() []ListType {
+	listTypes := make([]ListType, 0)
+	return listTypes
+}
+
+type ListType struct {
+	Name string `json:"name"`
+}
+
+func (lt *ListType) Validate() error {
+	if lt.Name == "" {
+		return fmt.Errorf("name is required")
+	}
+	return nil
+}
+
+type DownloadFile struct {
+	Name      string `json:"name"`
+	Folder    string `json:"folder"`
+	Filename  string `json:"filename"`
+	IsArchive bool   `json:"is_archive"`
+	URL       string `json:"url"`
+}
---
+++ internal/downloaders/downloader.go
@@ -0,0 +1,22 @@
+package downloaders
+
+import (
+	c "github.com/phani-kb/dns-toolkit/internal/common"
+	"github.com/phani-kb/multilog"
+)
+
+type Downloader interface {
+	// Download is a method to implement the logic to download the file
+	Download(
+		logger *multilog.Logger,
+		file c.DownloadFile,
+	) (string, bool, error)
+
+	// PostDownloadProcess is a method to implement the logic to consider the given count of entries after the download
+	PostDownloadProcess(logger *multilog.Logger, filePath string, count int) error
+
+	// Name is a method to return the name of the downloader
+	Name() string
+
+	ShouldDownload(logger *multilog.Logger, summaryFile string, file c.DownloadFile) bool
+}
